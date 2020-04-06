14e5d2a8c5696b62db3539e8c4ab9f6fc417ff3e
Fred Silveira <fredericos@eu.square-enix.com>
2020-04-06 02:20:38 +0100

    Created SearchBox and Data filter using React State

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Styles.css";
 
-const Search = () => {
+const Search = ({ searchChange }) => {
   return (
     <div className="searchBoxContainer">
-      <input type="search" placeholder="Find your dude" className="searchBox" />
+      <input
+        type="search"
+        placeholder="Find your dude"
+        className="searchBox"
+        onChange={searchChange}
+      />
     </div>
   );
 };
